6d7b1fde8b2f3c07ef29e92f73936db45212fdc5
dan5 <dan5.ya@gmail.com>
2012-01-06 17:24:33 +0900

    atndまわりのテキストをカラーリング

--- app/models/user_event.rb
+++ app/models/user_event.rb
@@ -2,4 +2,12 @@ class UserEvent < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
   validates_uniqueness_of :user_id, :scope => [:event_id]
+
+  def state_level
+    {
+      'attendance' => 1,
+      'maybe'      => 2,
+      'absence'    => 3,
+    }[state]
+  end
 end
