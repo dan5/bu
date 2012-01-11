aa66ed0c848da962b5862ebc3faffa4e1f1257ca
dan5 <dan5.ya@gmail.com>
2012-01-11 12:57:29 +0900

    permissionを追加

--- db/migrate/20111229051139_create_groups.rb
+++ db/migrate/20111229051139_create_groups.rb
@@ -4,6 +4,7 @@ class CreateGroups < ActiveRecord::Migration
       t.integer :owner_user_id
       t.string :name
       t.text :description
+      t.integer :permission, :default => 0 # 0:public / 1:need invitation / 2:secret
 
       t.timestamps
     end
