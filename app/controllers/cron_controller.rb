288913795d50cd7932973071be36a75d5c511834
dan5 <dan5.ya@gmail.com>
2012-01-12 23:11:16 +0900

    event終了条件をended_atを山容するように

--- app/controllers/cron_controller.rb
+++ app/controllers/cron_controller.rb
@@ -1,6 +1,6 @@
 class CronController < ApplicationController
   def update
-    events = Event.find(:all, :conditions => ["created_at < ? and ended = ?", Time.now, false])
+    events = Event.find(:all, :conditions => ["ended_at < ? and ended = ?", Time.now, false])
     events.each do |event|
       event.update_attributes(:ended => true)
       #event.atnds.each do |atnd|
