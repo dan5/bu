59b10f73d203e7fae5baa5708c64c7c137006244
TAKAHASHI Kazunari <ktakahashi@aiming-inc.com>
2012-06-28 20:39:46 +0900

    add groups_spec

---
+++ spec/factories/group.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :group  do
+    name 'Yokohama.rb'
+    summary 'in Yokohama'
+  end
+end
--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe "Groups" do
@@ -15,10 +16,57 @@ describe "Groups" do
     visit "/auth/twitter"
   end
 
+  describe "GET /groups/1" do
+    let!(:group) { FactoryGirl.create(:group) }
+    before do
+      Group.any_instance.stub(:owner?).and_return( true )
+      visit group_path(group)
+    end
+
+    it { page.should have_content(group.name) }
+
+    context "GET /group/1/edit" do
+      before { click_link 'Edit' }
+      it { page.current_path.should eq edit_group_path(group) }
+    end
+
+    context "DELETE /group/1" do
+      before { click_link 'Destroy' }
+      it { page.current_path.should eq group_path(group) + '/destroy_confirm' }
+    end
+  end
+
   describe "GET /groups/new" do
+    let!(:name){'Yokohama.rb'}
     before do
       visit new_group_path
+      fill_in 'group[name]', with: name
+      fill_in 'group[summary]', with: '横浜を中心とする…'
+      click_on 'Save'
     end
-    it { page.should have_xpath('//*[@id="group_name"]') }
+    it { page.should have_content(name) }
+  end
+
+  describe "GET /groups/1/edit" do
+    let!(:name){'Yokoshima.rb'}
+    let!(:group) { FactoryGirl.create(:group) }
+    before do
+      Group.any_instance.stub(:owner?).and_return( true )
+      visit edit_group_path(group)
+      fill_in 'group[name]', with: name
+      fill_in 'group[summary]', with: '横浜を中心とする…'
+      click_on 'Save'
+    end
+    it { page.should have_content(name) }
+  end
+
+  describe "DELETE /groups/1" do
+    let!(:group) { FactoryGirl.create(:group) }
+    before do
+      visit groups_path
+      Group.any_instance.stub(:owner?).and_return( true )
+      click_link 'Destroy'
+    end
+    it { page.should have_content('Group was successfully deleted.') }
   end
 end
