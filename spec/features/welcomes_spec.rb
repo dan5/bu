3e4abd7dafd391fe10a4fc46249d8221dda37579
TAKAHASHI Kazunari <takahashi@1syo.net>
2012-12-11 20:15:06 +0900

    root_pathにグループ一覧される検証を追加した

--- spec/features/welcomes_spec.rb
+++ spec/features/welcomes_spec.rb
@@ -6,10 +6,20 @@ describe "Welcomes" do
     before { visit root_path }
 
     it { page.should have_content('Bu: beta') }
+  end
 
-    describe "click new groups" do
-      before { click_link "新しい部活を作る" }
-      it { page.should have_content('Login') }
+  describe "click new groups" do
+    before do
+      visit root_path
+      click_link "新しい部活を作る"
     end
+    it { page.should have_content('Login') }
+  end
+
+  describe "listing groups" do
+    include_context "twitter_login"
+    include_context 'visit_group_page'
+    before { visit root_path }
+    it { page.should have_content(group.name) }
   end
 end
