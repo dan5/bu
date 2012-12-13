b0e0ea3cfd57ceb1c9ca02e3e0a85075575e6fc4
miyohide <miyohide@gmail.com>
2012-12-13 22:42:13 +0900

    add 'do not enter mail address' test pattern

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -8,15 +8,32 @@ describe "Users" do
     let(:new_user) do
       FactoryGirl.attributes_for(:user, name: Forgery::Basic.text, mail: Forgery::Email.address)
     end
+    
+    context 'すべての領域に値を入力' do
+      before do
+        visit users_edit_path
+        fill_in 'user[name]', with: new_user[:name]
+        fill_in 'user[mail]', with: new_user[:mail]
+        click_on 'Save'
+      end
 
-    before do
-      visit users_edit_path
-      fill_in 'user[name]', with: new_user[:name]
-      fill_in 'user[mail]', with: new_user[:mail]
-      click_on 'Save'
+      it { page.should have_content('User was successfully updated.') }
+      it { page.should_not have_content('Please input your mail address') }
     end
 
-    it { page.should have_content('User was successfully updated.') }
-    it { page.should_not have_content('Please input your mail address') }
+    context 'name領域だけ入力されているとき' do
+      before do
+        visit users_edit_path
+        fill_in 'user[name]', with: new_user[:name]
+        fill_in 'user[mail]', with: ''
+        click_on 'Save'
+      end
+
+      it 'Userの更新は失敗する？' do
+        pending '仕様が不明なのですが、現状ではアップデートが成功します。これで良い？'
+        page.should_not have_content('User was successfully updated.')
+      end
+      it { page.should have_content('Please input your mail address') }
+    end
   end
 end
