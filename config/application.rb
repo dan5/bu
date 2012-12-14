1d5752ea749503fbc36cd4b30f0bb5dc05e91cf1
TAKAHASHI Kazunari <takahashi@1syo.net>
2012-12-14 15:05:18 +0900

    active_resourceの依存を削除

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "active_resource/railtie"
 require "sprockets/railtie"
 
 if defined?(Bundler)
