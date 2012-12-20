990af45fd9450956b4d6a077c54983b695cd2321
TAKAHASHI Kazunari <takahashi@1syo.net>
2012-12-21 00:06:41 +0900

    createとupdateのspecを作成した

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,9 +2,6 @@
 require 'spec_helper'
 
 describe Group do
-  let(:you) { FactoryGirl.create(:user) }
-  subject { FactoryGirl.create(:group, owner_user_id: you.id) }
-
   describe 'Validations' do
     it { should validate_presence_of(:name).with_message(/can't be blank/)  }
     it { should validate_presence_of(:summary).with_message(/can't be blank/)  }
@@ -13,7 +10,7 @@ describe Group do
     pending { should ensure_length_of(:description).is_at_least(4096).with_message(/not long enough/) }
   end
 
-  describe 'Association' do
+  describe 'Associations' do
     it { should have_many(:events) }
     it { should have_many(:posts) }
     it { should have_many(:user_groups) }
@@ -22,7 +19,33 @@ describe Group do
     it { should have_many(:requesting_users) }
   end
 
+  describe "#create" do
+    let(:you) { FactoryGirl.create(:user) }
+    let!(:group) { FactoryGirl.create(:group, owner_user_id: you.id) }
+    it { group.users.should include you }
+  end
+
+  describe "#update" do
+    let(:you) { FactoryGirl.create(:user) }
+    let(:current) { FactoryGirl.create(:group, owner_user_id: you.id, permission: permission) }
+    let!(:member_request) { FactoryGirl.create(:member_request, group_id: current.id) }
+
+    let(:edited) { FactoryGirl.attributes_for(:group) }
+
+    context 'public group' do
+      let(:permission) { 0 }
+      it { expect{ current.update_attributes(edited) }.to change(MemberRequest, :count).by(-1) }
+    end
+
+    context 'not public group' do
+      let(:permission) { 1 }
+      it { expect{ current.update_attributes(edited) }.to change(MemberRequest, :count).by(0) }
+    end
+  end
+
   describe "#destroy" do
+    let(:you) { FactoryGirl.create(:user) }
+
     context 'メンバーが1名以下の場合は削除できる' do
       let!(:group) { FactoryGirl.create(:group, owner_user_id: you.id) }
       it { expect{ group.destroy }.to change(Group, :count).by(-1) }
