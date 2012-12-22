f8b470df3e2103e951926a2eb2fa73442ca69748
TAKAHASHI Kazunari <takahashi@1syo.net>
2012-12-22 14:24:56 +0900

    一時変数を削除した

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,19 @@
-# thx: http://d.hatena.ne.jp/kaorumori/20111113/1321155791
 class SessionsController < ApplicationController
   def callback
     user = User.find_or_create_with_omniauth(request.env['omniauth.auth'])
     session[:user_id] = user.id
-    path = session.delete(:redirect_path) || '/my'
-    redirect_to path, notice: 'Login successful.'
+
+    redirect_to redirect_path, notice: 'Login successful.'
   end
 
   def destroy
     session[:user_id] = nil
+
     redirect_to root_path
+  end
+
+  private
+  def redirect_path
+    session.delete(:redirect_path) || my_url
   end
 end
