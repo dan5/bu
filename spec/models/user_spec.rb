c508e0aa66a86ba31761b0b39bc26ae9a2d97480
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-12-23 17:33:34 +0900

    Add validations/associations spec for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,21 @@
 require 'spec_helper'
 
 describe User do
+  describe "Validations" do
+    # validate_uniqueness_of matcher は既存のレコードが必要
+    before { FactoryGirl.create(:user) }
+    it { should validate_presence_of(:name).with_message(/can't be blank/) }
+    it { should ensure_length_of(:name).is_at_most(16).with_long_message(/too long/) }
+    it { should validate_uniqueness_of(:uid).scoped_to(:provider).with_message(/already been taken/) }
+  end
+
+  describe "Associations" do
+    it { should have_many(:comments) }
+    it { should have_many(:groups).through(:user_groups) }
+    it { should have_many(:events).through(:user_events) }
+    it { should have_many(:requested_groups).through(:member_requests) }
+  end
+
   describe '.find_or_create_with_omniauth' do
     subject { User.find_or_create_with_omniauth(auth) }
 
