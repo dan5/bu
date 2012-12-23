59ef27f150f492532a3230b1710ac10bd4e1730d
TAKAHASHI Kazunari <takahashi@1syo.net>
2012-12-23 11:31:26 +0900

    user_groupのテストを書いた

---
+++ spec/controllers/user_groups_controller_spec.rb
@@ -0,0 +1,47 @@
+# coding: utf-8
+require 'spec_helper'
+
+describe UserGroupsController do
+  let!(:you) { FactoryGirl.create(:user) }
+  let!(:member) { FactoryGirl.create(:user) }
+  let!(:group) { FactoryGirl.create(:group, owner_user_id: owner.id) }
+  let!(:user_group) { FactoryGirl.create(:user_group, group_id: group.id, user_id: member.id) }
+  let(:edited_user_group) { FactoryGirl.attributes_for(:user_group, group_id: group.id, user_id: member.id) }
+
+  before { request.session[:user_id] = you.id }
+
+  describe "PUT update" do #groupにおける役職名の設定
+    describe "with valid params" do
+      context "Managerの場合は更新できること" do
+        let(:owner) { you }
+        before { put :update, { id: user_group.to_param, user_group:  edited_user_group } }
+        it { response.should redirect_to(group_users_url(group_id: group.id)) }
+      end
+
+      context "Managerでない場合は更新できないこと" do
+        let(:owner) { FactoryGirl.create(:user) }
+        before { bypass_rescue }
+        it { expect { put :update, {id: user_group.to_param, user_group: edited_user_group } }.to raise_error(Group::NotGroupManager) }
+      end
+    end
+
+    describe "with invalid params" do
+      let(:owner) { you }
+      before { put :update, {id: user_group.to_param, user_group: {}} }
+      it { response.should redirect_to(group_users_url(group_id: group.id)) }
+    end
+  end
+
+  describe "DELETE destroy" do
+    context 'Ownerの場合は削除できること' do
+      let(:owner) { you }
+      it { expect { delete :destroy, {id: user_group.to_param} }.to change(UserGroup, :count).by(-1) }
+    end
+
+    context 'Ownerじゃない場合は削除できないこと' do
+      let(:owner) { FactoryGirl.create(:user) }
+      before { bypass_rescue }
+      it { expect { delete :destroy, {id: user_group.to_param} }.to raise_error(Group::NotGroupManager) }
+    end
+  end
+end
--- spec/factories/user_group.rb
+++ spec/factories/user_group.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :user_group  do
     user_id { Forgery::Basic.number }
     group_id { Forgery::Basic.number }
+    state { Forgery::Basic.text }
+    role { Forgery::Basic.text }
   end
 end
