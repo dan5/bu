804040d9fc96f56d2fd2200e2b43bdc981432028
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-12-23 23:18:13 +0900

    Modify spec for UserGroupsController#destroy

--- spec/controllers/user_groups_controller_spec.rb
+++ spec/controllers/user_groups_controller_spec.rb
@@ -2,15 +2,15 @@
 require 'spec_helper'
 
 describe UserGroupsController do
-  let!(:you) { FactoryGirl.create(:user) }
-  let!(:member) { FactoryGirl.create(:user) }
-  let!(:group) { FactoryGirl.create(:group, owner_user_id: owner.id) }
-  let!(:user_group) { FactoryGirl.create(:user_group, group_id: group.id, user_id: member.id) }
-  let(:edited_user_group) { FactoryGirl.attributes_for(:user_group, group_id: group.id, user_id: member.id) }
-
-  before { request.session[:user_id] = you.id }
-
   describe "PUT update" do #groupにおける役職名の設定
+    let!(:you) { FactoryGirl.create(:user) }
+    let!(:member) { FactoryGirl.create(:user) }
+    let!(:group) { FactoryGirl.create(:group, owner_user_id: owner.id) }
+    let!(:user_group) { FactoryGirl.create(:user_group, group_id: group.id, user_id: member.id) }
+    let(:edited_user_group) { FactoryGirl.attributes_for(:user_group, group_id: group.id, user_id: member.id) }
+
+    before { request.session[:user_id] = you.id }
+
     context "with valid params" do
       context "Managerの場合は更新できること" do
         let(:owner) { you }
@@ -39,21 +39,65 @@ describe UserGroupsController do
   end
 
   describe "DELETE destroy" do
-    context 'Ownerの場合は削除できること' do
-      let(:owner) { you }
-      it { expect { delete :destroy, {id: user_group.to_param} }.to change(UserGroup, :count).by(-1) }
+    let(:owner) { FactoryGirl.create(:user) }
+    let(:target) { FactoryGirl.create(:user) }
+    let(:group) { FactoryGirl.create(:group, owner_user_id: owner.id) }
+    let(:target_user_groups_id) { target.user_groups.where(:group_id => group.id).first.to_param }
+
+    before do
+      request.session[:user_id] = operator.id
     end
 
-    context 'Ownerじゃない場合は削除できないこと' do
-      let(:owner) { FactoryGirl.create(:user) }
-      before { bypass_rescue }
-      it { expect { delete :destroy, {id: user_group.to_param} }.to raise_error(Group::NotGroupManager) }
+    context '操作者がOwnerのとき' do
+      let(:operator) { owner }
+      before do
+        group.users << target
+      end
+      it '対象者が削除されること' do
+        expect {
+          delete :destroy, {id: target_user_groups_id}
+        }.to change(UserGroup, :count).by(-1)
+      end
     end
 
-    context 'Owner自身は削除できること' do
-      let(:owner) { you }
-      let(:user_group_id) { UserGroup.where(user_id: you.id, group_id: group.id).first }
-      it { expect { delete :destroy, {id: user_group_id} }.to change(UserGroup, :count).by(0) }
+    context '操作者がManagerのとき' do
+      let(:operator) { FactoryGirl.create(:user) }
+      before do
+        # managerにする
+        FactoryGirl.create(:user_group, user_id: operator.id, group_id: group.id, role: 'role')
+      end
+      context '対象者がOwnerのとき' do
+        let(:target) { owner }
+        it '対象者が削除されないこと' do
+          expect {
+            delete :destroy, {id: target_user_groups_id}
+          }.not_to change(UserGroup, :count)
+        end
+      end
+      context '対象者がOwnerではないとき' do
+        before do
+          group.users << target
+        end
+        it '対象者が削除されること' do
+          expect {
+            delete :destroy, {id: target_user_groups_id}
+          }.to change(UserGroup, :count).by(-1)
+        end
+      end
+    end
+
+    context '操作者がOwnerでもManagerでもないとき' do
+      let(:operator) { FactoryGirl.create(:user) }
+      before do
+        bypass_rescue
+        group.users << operator
+        group.users << target
+      end
+      it 'Group::NotGroupManagerが発生すること' do
+        expect {
+          delete :destroy, {id: target_user_groups_id}
+        }.to raise_error(Group::NotGroupManager)
+      end
     end
   end
 end
