5feb9c29b2fc0ac272399af57ac7096b0922f0ad
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-01-07 20:47:37 +0900

    add spec for request_to_join

--- spec/controllers/concerns/members_spec.rb
+++ spec/controllers/concerns/members_spec.rb
@@ -66,4 +66,52 @@ describe Members do
       it { expect { get :leave, id: group.to_param }.to change(UserGroup, :count).by(0) }
     end
   end
+
+  describe '#request_to_join' do
+    let(:you) { FactoryGirl.create(:user) }
+
+    before do
+      @routes.draw { get "anonymous/:id/request_to_join" => "anonymous#request_to_join" }
+    end
+
+    context 'ログインしていない場合' do
+      let!(:group) { FactoryGirl.create(:group, owner_user_id: you.id) }
+
+      before { bypass_rescue }
+      it { expect { get :request_to_join, id: group.to_param }.to raise_error(User::UnAuthorized) }
+    end
+
+    context 'ログインしている場合' do
+      let!(:group) { FactoryGirl.create(:group, owner_user_id: you.id, permission: permission) }
+
+      context 'グループがpublicである場合' do
+        let(:permission) { 0 }
+        it { expect { get :request_to_join, id: group.to_param }.to change(MemberRequest, :count).by(0) }
+      end
+
+      context 'グループがpublicでない場合' do
+        let(:permission) { 1 }
+
+        context 'ユーザーがメンバーの場合' do
+          before { login_as(you) }
+          it { expect { get :request_to_join, id: group.to_param }.to change(MemberRequest, :count).by(0) }
+        end
+
+        context 'ユーザーがメンバーリクエスト済の場合' do
+          let(:other) { FactoryGirl.create(:user) }
+          before do
+            login_as(other)
+            group.requesting_users << other
+          end
+          it { expect { get :request_to_join, id: group.to_param }.to change(MemberRequest, :count).by(0) }
+        end
+
+        context 'ユーザーがメンバーではない場合' do
+          let(:other) { FactoryGirl.create(:user) }
+          before { login_as(other) }
+          it { expect { get :request_to_join, id: group.to_param }.to change(MemberRequest, :count).by(+1) }
+        end
+      end
+    end
+  end
 end
