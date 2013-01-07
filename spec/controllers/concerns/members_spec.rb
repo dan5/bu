6de178242c346c30fe96efd35cd195f1f1c96e2c
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-01-07 19:49:11 +0900

    add spec for join method

--- spec/controllers/concerns/members_spec.rb
+++ spec/controllers/concerns/members_spec.rb
@@ -6,14 +6,44 @@ describe Members do
 
   controller do
     include Members
-    def index
-      render :nothing => true
-    end
   end
 
-  context '#join' do
+  describe '#join' do
+    before do
+      @routes.draw { get "anonymous/:id/join" => "anonymous#join" }
+    end
+
     let(:you) { FactoryGirl.create(:user) }
-    let!(:group) { FactoryGirl.create(:group, owner_user_id: you.id) }
-    let(:user) { FactoryGirl.create(:user) }
+
+    context 'ログインしていない場合' do
+      let!(:group) { FactoryGirl.create(:group, owner_user_id: you.id) }
+
+      before { bypass_rescue }
+      it { expect { get :join, id: group.to_param }.to raise_error(User::UnAuthorized) }
+    end
+
+    context 'ログインしている場合' do
+      let!(:group) { FactoryGirl.create(:group, owner_user_id: you.id, permission: permission) }
+
+      context 'グループがpublicである場合' do
+        let(:permission) { 0 }
+
+        context 'ユーザーがメンバーの場合' do
+          before { login_as(you) }
+          it { expect { get :join, id: group.to_param }.to change(UserGroup, :count).by(0) }
+        end
+
+        context 'ユーザーがメンバーではない場合' do
+          let(:other) { FactoryGirl.create(:user) }
+          before { login_as(other) }
+          it { expect { get :join, id: group.to_param }.to change(UserGroup, :count).by(+1) }
+        end
+      end
+
+      context 'グループがpublicでない場合' do
+        let(:permission) { 1 }
+        it { expect { get :join, id: group.to_param }.to change(UserGroup, :count).by(0) }
+      end
+    end
   end
 end
