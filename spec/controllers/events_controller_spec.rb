566adc5e8aa7ebb7e5dcd52ceba961160b0293cc
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-01-10 12:25:15 +0900

    add spec for #show

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,4 +2,11 @@
 require 'spec_helper'
 
 describe EventsController do
+  describe "GET 'show'" do
+    let!(:group) { FactoryGirl.create(:group) }
+    let!(:event) { FactoryGirl.create(:event, group_id: group.id) }
+
+    before { get :show, id: group.to_param }
+    it { response.should be_success }
+  end
 end
