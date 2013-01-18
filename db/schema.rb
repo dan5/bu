62fc2ea4eea53460dffceda213cb3c88c002933e
miyohide <miyohide@gmail.com>
2013-01-19 00:15:42 +0900

    add index to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130118150642) do
+ActiveRecord::Schema.define(:version => 20130118151213) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -75,6 +75,9 @@ ActiveRecord::Schema.define(:version => 20130118150642) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
+
+  add_index "posts", ["group_id"], :name => "index_posts_on_group_id"
+  add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
 
   create_table "user_events", :force => true do |t|
     t.integer  "user_id"
