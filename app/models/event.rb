94dee8ddb1038a5fba5d184604d23dcf75b1826d
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-01-30 19:42:46 +0900

    add spec for event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -96,6 +96,6 @@ class Event < ActiveRecord::Base
   def self.be_ended_all
     t = Time.now
     events = where("started_at < ? and ended_at < ? and ended = ?", t, t, false)
-    events.each &:be_ended
+    events.each(&:be_ended)
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -12,4 +12,36 @@ describe Event do
     it { should have_many(:comments) }
     it { should have_many(:users).through(:user_events) }
   end
+
+  describe '.be_ended_all' do
+    let(:group) { FactoryGirl.create(:group, limit: 1) }
+    let!(:events) { FactoryGirl.create_list(:event, 10, started_at: 1.day.ago, ended_at: 1.week.ago) }
+
+    before do
+      @count = 0
+      Event.any_instance.stub(:be_ended) { @count += 1 }
+      Event.be_ended_all
+    end
+
+    subject { @count }
+
+    it { should be events.count }
+  end
+
+  describe '.be_ended' do
+    let(:group) { FactoryGirl.create(:group) }
+    let(:event) { FactoryGirl.create(:event, group_id: group.id, limit: 1) }
+    let(:you) { FactoryGirl.create(:user) }
+    let(:other) { FactoryGirl.create(:user) }
+
+    before do
+      [you, other].each do |user|
+        FactoryGirl.create(:user_event, user_id: user.id, event_id: event.id, state: 'attendance')
+      end
+
+      event.be_ended
+    end
+
+    it { event.ended.should be_true }
+  end
 end
