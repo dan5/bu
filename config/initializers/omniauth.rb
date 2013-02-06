b0a51b533aac0c50abd21eeaf10ca4c0b3aa9c9e
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-02-06 14:29:14 +0900

    add setting

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'cHLNdPK4BPoXtghkowfjLw', 'snrURdBxphS3BOg4HK5ozBRzUThm6fGUTpwyG55NlSE'
+  provider :twitter, (ENV['TWITTER_KEY'] || 'cHLNdPK4BPoXtghkowfjLw'), (ENV['TWITTER_SECRET'] || 'snrURdBxphS3BOg4HK5ozBRzUThm6fGUTpwyG55NlSE')
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bu::Application.config.secret_token = 'f62a63ed98ea795768053e15b4af4eb7081b4c1c2aca9ac46ca63c255a072f8674fd6532a731145e8946069ece073b6f083df6fd96fbe4bd7d2b9850cb0c81e3'
+Bu::Application.config.secret_token = (ENV['SECRET_TOKEN'] || 'f62a63ed98ea795768053e15b4af4eb7081b4c1c2aca9ac46ca63c255a072f8674fd6532a731145e8946069ece073b6f083df6fd96fbe4bd7d2b9850cb0c81e3')
